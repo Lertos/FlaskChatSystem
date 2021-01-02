e4493bae5a75c5c08801a6d640d6b929f16b7f74
Lertos <deetasticgaming@gmail.com>
2021-01-02 14:46:26 -0400

    Adding handling for POST requests

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
 def main():
-    return render_template("index.html")
+    #If it was a GET request, simply display the page
+    if request.method == "GET":
+        return render_template("index.html", comments=comments)
+
+    #If the request was not a GET - add the text inside of the 'contents' form to our list
+    comments.append(request.form["contents"])
+    #Says: Please request this page again, this time using a 'GET' method (default request)
+    return redirect(url_for('main'))
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
